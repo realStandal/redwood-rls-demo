1eadbd8da56a57bb99a6d314ec07c4b6aa8d6688
realStandal <jesselockard99@gmail.com>
2023-02-07 17:55:05 -0500

    relocated web-side auth library

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,9 @@
 import { FatalErrorBoundary, RedwoodProvider } from '@redwoodjs/web'
 import { RedwoodApolloProvider } from '@redwoodjs/web/apollo'
 
+import { AuthProvider, useAuth } from 'src/lib/auth'
 import FatalErrorPage from 'src/pages/FatalErrorPage'
 import Routes from 'src/Routes'
-
-import { AuthProvider, useAuth } from './auth'
 
 import './index.css'
 
--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -1,15 +1,6 @@
-// In this file, all Page components from 'src/pages` are auto-imported. Nested
-// directories are supported, and should be uppercase. Each subdirectory will be
-// prepended onto the component name.
-//
-// Examples:
-//
-// 'src/pages/HomePage/HomePage.js'         -> HomePage
-// 'src/pages/Admin/BooksPage/BooksPage.js' -> AdminBooksPage
-
 import { Router, Route } from '@redwoodjs/router'
 
-import { useAuth } from './auth'
+import { useAuth } from 'src/lib/auth'
 
 const Routes = () => {
   return (
