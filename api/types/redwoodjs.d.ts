ef336bf99c1c098b3ba1dfebad09cebf3289ab8a
realStandal <jesselockard99@gmail.com>
2023-02-08 23:13:58 -0500

    relocated "@redwoodjs/graphql-server" type declarations

--- api/types/redwoodjs.d.ts
+++
@@ -1,7 +0,0 @@
-import type { getAuthDb } from 'src/lib/db'
-
-declare module '@redwoodjs/graphql-server' {
-  interface GlobalContext {
-    prisma: ReturnType<typeof getAuthDb>
-  }
-}
---
+++ types/redwoodjs.d.ts
@@ -0,0 +1,10 @@
+import type { getAuthDb } from 'src/lib/db'
+
+declare module '@redwoodjs/graphql-server' {
+  interface GlobalContext {
+    /**
+     * When a request is made by an authenticated user, this will be an [extended Prisma Client](https://www.prisma.io/docs/concepts/components/prisma-client/client-extensions) which is associated with the user and its tenant. When a request is made by an anonymous user, the default Prisma Client is included instead.
+     */
+    prisma: ReturnType<typeof getAuthDb>
+  }
+}
