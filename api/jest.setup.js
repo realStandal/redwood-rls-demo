ec57da2db7dcd933113592d8da255176584161db
Ryan Lockard <25166787+realStandal@users.noreply.github.com>
2023-02-14 17:39:35 -0500

    Create jest.setup.js

---
+++ api/jest.setup.js
@@ -0,0 +1,22 @@
+// @ts-check
+const { getDMMF } = require('@prisma/internals')
+
+const { db } = require('./src/lib/db')
+
+const { dbSchemaPath } = global.__RWJS__TEST_IMPORTS
+
+let TEARDOWN_ORDER = []
+
+beforeAll(async () => {
+  const schema = await getDMMF({ datamodelPath: dbSchemaPath })
+  TEARDOWN_ORDER = schema.datamodel.models.map((m) => m.dbName || m.name)
+})
+
+afterEach(async () => {
+  for (const modelName of TEARDOWN_ORDER) {
+    await db.$executeRawUnsafe('BEGIN')
+    await db.$executeRawUnsafe("SELECT set_config('app.bypass', 'on', TRUE)")
+    await db.$executeRawUnsafe(`DELETE FROM "${modelName}"`)
+    await db.$executeRawUnsafe('COMMIT')
+  }
+})
